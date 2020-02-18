b2580a8d8892eae01de4ebb10fa709149298d777
Ubuntu <ubuntu@ip-172-31-33-234.us-east-2.compute.internal>
2020-02-18 04:43:34 +0000

    inital commit

--- code-challenges.js
+++ code-challenges.js
@@ -3,8 +3,9 @@
 // --------------------1) Create a function that takes a number as an argument and decides if the number is evenly divisble by three or not.
 // Use the test variables provided.
 
+
 var num1 = 15
-// Expected output: "15 is divisible by three"
+// Expected output: "15 is divisible by three" ,
 
 var num2 = 0
 // Expected output: "0 is divisible by three"
@@ -12,6 +13,16 @@ var num2 = 0
 var num3 = -7
 // Expected output: "-7 is not divisble by three"
 
+const evenDivide = (num) => {
+    if (num % 3 === 0) {
+        return `${num} is divisible by three`
+    }       else {
+        return `${num} is not divisible by three`
+    }
+}
+    console.log(evenDivide(num1))
+    console.log(evenDivide(num2))
+    console.log(evenDivide(num3))
 
 
 
@@ -21,7 +32,10 @@ var num3 = -7
 var randomNouns = ["streetlamp", "potato", "teeth", "conclusion", "nephew", "temperature", "database"]
 // Expected output: ["Streetlamp", "Potato", "Teeth", "Conclusion", "Nephew", "Temperature", "Database"]
 
-
+const wordsCapitalized = (array) => {
+   return array.map(value => value.charAt(0).toUpperCase() + value.substr(1));
+}
+console.log(wordsCapitalized(randomNouns))
 
 
 
@@ -30,8 +44,13 @@ var randomNouns = ["streetlamp", "potato", "teeth", "conclusion", "nephew", "tem
 var mixedDataArray = [true, 8, "hello", 90, -8, null, 0, 46, 59, 107, "hey!"]
 // Expected output: [-8, 0, 8, 46, 59, 90, 107]
 
+const onlyNumbers = (array) => {
+    let newArr = []
+    newArr = array.sort((a,b) => a - b).filter(value => typeof value === "number")
+     return newArr 
 
-
+}
+console.log(onlyNumbers(mixedDataArray))
 
 
 // --------------------4) Create a function that takes in a string and logs the index of the first vowel.
@@ -39,9 +58,21 @@ var mixedDataArray = [true, 8, "hello", 90, -8, null, 0, 46, 59, 107, "hey!"]
 
 var vowelTester1 = "learn"
 // Expected output: 1
-var vowelTester1 = "throw"
+var vowelTester2 = "throw"
 // Expected output: 3
 
+// create a function called vowelIndex that takes in a string and logs the index of the first vowel,
+// create an array called vowelArray that contains only vowel letters.
+
+const vowelIndex = (str) => {
+   let vowelArray = ["a", "e", "i", "o", "u"]
+    str.split("")
+        for (let i= 0; i <str.length; i++)
+            if (vowelArray.includes(str[i])) {
+                return i
+            }
+console.log(vowelIndex(vowelTester1))
+console.log(vowelIndex(vowelTester2))
 
 
 
@@ -49,16 +80,29 @@ var vowelTester1 = "throw"
 
 // --------------------5) Create a function that takes three arguments - two numbers and a mathematical operation (+, -, *, /) and performs the given calculation. If the input tries to divide by 0, return: "Can't divide by 0!"
 
-
+const calculator = (num1, str, num2) => {
+    if (str === "+") {
+        return num1 + num2 
+    }   else if (str === "-") {
+        return num1 - num2
+    }   else if (str === "*") {
+        return num1 * num2
+    }   else if (str === "/") {
+        return num1 / num2
+    }   else if (str === "/" && num2 === 0) {
+        return "Can't divide by 0!"
+    }
+}
 
 
 
 // Uncomment and use the following console logs to test your function
-// console.log(calculator(3, "*" 9))
+console.log(calculator(3, "*", 9)) 
 // Expected output: 27
 
-// console.log(calculator(16, "+" 3))
+console.log(calculator(16, "+", 3))
 // Expected output: 19
 
-// console.log(calculator(89, "/" 0))
+console.log(calculator(89, "/", 0))
 // Expected output: "Can't divide by 0!"
+
